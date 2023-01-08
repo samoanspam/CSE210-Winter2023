ebabe792a72a32b26147ed42bf1bc54841fc0beb
samoanspam <thalpatane@byui.edu>
2023-01-07 20:45:34 -0700

    Tons of changes, requirements up to 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        if (gradePercentage > 90)
+        {
+            string finalGrade = "A";
+            Console.WriteLine($"Your grade is an {finalGrade}. Amazing Job! ");
+        }
+        else if (gradePercentage > 80)
+        {
+            string finalGrade = "B";
+            Console.WriteLine($"Your grade is an {finalGrade}. Good Job! ");
+        }
+        else if (gradePercentage > 70)
+        {
+            string finalGrade = "C";
+            Console.WriteLine($"Your grade is an {finalGrade}. Alright Job! ");
+        }
+        else if (gradePercentage > 60)
+        {
+            string finalGrade = "D";
+            Console.WriteLine($"Your grade is an {finalGrade}. Not Good Job! ");
+        }
+        else
+        {
+            string finalGrade = "F";
+            Console.WriteLine($"Your grade is an {finalGrade}. Bad Job! ");
+        }
+
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congradulations, you passed the class! ");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you failed, try again next semester! ");
+        }
     }
 }
