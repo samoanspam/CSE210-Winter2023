78d7ef6ba10681955aec7644e3226ee2c3f8b269
samoanspam <thalpatane@byui.edu>
2023-01-07 20:18:03 -0700

    Name input and formatted print

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"Your color is {color}. ");
+        // Console.WriteLine("Hello World!");
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+        // Console.WriteLine($"Your color is {color}. ");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.Write("");
+        Console.Write($"Nice to meet you {firstName} {lastName}! I'm Teia's Computer. ");
     }
 }
