b521d1e43bcea1acb1a16f9b602cfd89a2db11a9
samoanspam <thalpatane@byui.edu>
2023-01-11 20:22:01 -0700

    The entire prep 3 plus the stretch.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    Random randomGenerator = new Random();
+
+    int actualNumber = randomGenerator.Next(1,101);
+    int guess = -1;
+    int count = 1;
+    string playAgain = "yes";
+    
+    while (playAgain == "yes")
+    {
+        while (guess != actualNumber)
+        {
+            Console.Write("Guess a number 1-100: ");
+            guess = int.Parse(Console.ReadLine());
+            if (actualNumber > guess)
+            {
+                Console.WriteLine("Higher! ");
+            }
+            else if (actualNumber < guess)
+            {
+                Console.WriteLine("Lower! ");
+            }
+            else
+            {
+                Console.WriteLine("Bingo, you guessed it! ");
+            }
+            count += 1;
+        }
+        Console.Write("Do you want to play again? ");
+        playAgain = Console.ReadLine().ToLower();
+    }
+    
+    Console.WriteLine($"It took you {count} time(s) to guess it! ");
     }
 }
