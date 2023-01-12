07d222e4b50aa5a48230103018a96f8ba3409dc3
samoanspam <thalpatane@byui.edu>
2023-01-11 21:18:16 -0700

    the core assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        while (guess != number)
+        {
+            if (guess > number)
+            {
+                Console.WriteLine("Lower! ");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher! ");
+            }
+            Console.Write("What is your guess? ");
+            string newGuess = Console.ReadLine();
+            guess = int.Parse(newGuess);
+        }
+        Console.WriteLine("You guessed it! ");
     }
 }
