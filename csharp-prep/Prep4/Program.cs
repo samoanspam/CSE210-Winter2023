c25195fcf259d817a64ccaf2f75d7441cabc1232
samoanspam <thalpatane@byui.edu>
2023-01-11 21:08:08 -0700

    getting stuck on line 19 but should work afterward

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        List<int> numbers;
+        numbers = new List<int>();
+        int inputInt = -1;
+
+        while (inputInt != 0)
+        {
+            Console.Write("Enter Number: ");
+            inputInt = int.Parse(Console.ReadLine());
+            numbers.Add(inputInt);
+        }
+        Console.WriteLine(numbers.Count - 1);
+        int listLength = int.Parse(Console.ReadLine());
+
+        // my code stops right here at line 19 and im not sure why
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of the list is {sum}. ");
+
+        float average = ((float)sum) / listLength;
+        Console.WriteLine($"The average of the list is about {average}. ");
+
+
+        int maximum = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine($"The largest number on the list is {maximum}. ");
     }
 }
