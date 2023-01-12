475c225a113fbfbd6588f5abf7547bb97b09c9e4
samoanspam <thalpatane@byui.edu>
2023-01-11 21:25:42 -0700

    stretch challenge

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,27 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        
-        int number = randomGenerator.Next(1,101);  
 
-        Console.Write("What is your guess? ");
-        string guessInput = Console.ReadLine();
-        int guess = int.Parse(guessInput);
+        bool keepPlaying = true;
 
-        while (guess != number)
+        while (keepPlaying != true)
         {
-            if (guess > number)
-            {
-                Console.WriteLine("Lower! ");
-            }
-            else if (guess < number)
-            {
-                Console.WriteLine("Higher! ");
-            }
+            int number = randomGenerator.Next(1,101);  
+
             Console.Write("What is your guess? ");
-            string newGuess = Console.ReadLine();
-            guess = int.Parse(newGuess);
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+            int count = 0;
+            while (guess != number)
+            {
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower! ");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher! ");
+                }
+                Console.Write("What is your guess? ");
+                string newGuess = Console.ReadLine();
+                guess = int.Parse(newGuess);
+
+                count += 1;
+            }
+            Console.WriteLine("You guessed it! ");
+            Console.WriteLine($"It took you {count} tries!");
+
+            Console.Write("Would you like to play again? ");
+            string keepPlayingInput = Console.ReadLine().ToLower();
+
+            if (keepPlayingInput == "yes")
+            {
+                keepPlaying = bool.Parse(keepPlayingInput);
+                keepPlaying = true;
+            }
+            else if (keepPlayingInput == "no")
+            {
+                keepPlaying = bool.Parse(keepPlayingInput);
+                keepPlaying = false;  
+            }
         }
-        Console.WriteLine("You guessed it! ");
     }
 }
