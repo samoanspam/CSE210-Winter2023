0e2c653ae701502b71515297649a5c7bd550a92d
samoanspam <thalpatane@byui.edu>
2023-01-18 22:12:43 -0700

    Step 4 and half of 5

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+public class Job
+{
+    public string _jobTitle = "";
+    public string _company = "";
+    public int _startYear;
+
+    public int _endYear;
+
+    public Job()
+    {
+    }
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}. Wow! ");
+    }
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Boss";
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        System.Console.WriteLine(job2._company);
+        job2._company = "Apple";
+        System.Console.WriteLine(job2._company);
+
+        System.Console.WriteLine(job1._jobTitle);
     }
 }
