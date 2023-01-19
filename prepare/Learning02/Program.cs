95c029f1ff07855c291110b0c4a21146c64881a6
samoanspam <thalpatane@byui.edu>
2023-01-18 22:19:12 -0700

    Step 5 and display

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,14 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Boss";
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;        
+
+        job1.DisplayJobDetails();
 
         Job job2 = new Job();
-        job2._company = "Microsoft";
-        System.Console.WriteLine(job2._company);
+        job2._jobTitle = "Manager";
         job2._company = "Apple";
-        System.Console.WriteLine(job2._company);
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        System.Console.WriteLine(job1._jobTitle);
+        job2.DisplayJobDetails();
     }
 }
