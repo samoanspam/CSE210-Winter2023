00a5d58d46e9f77b71c792027317e69e6d07837f
samoanspam <thalpatane@byui.edu>
2023-02-01 21:42:27 -0700

    test?

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,121 +1,118 @@
-using System;
-using System.Collections.Generic;
-class Journal
-{
-    public string JournalFile = "Journal.txt";
 
 
 
-public void Menu()
-{
-    string choice;
-    do
-    {
-        choice = MenuAction();
-        switch (choice)
-        {
-            case "1":
-                DislpayJournal();
-                break;
-            case "2":
-                DeleteJournal();
-                break;
-            case "3":
-                AddEntry();
-                break;
 
-        }
-    }
-    while (choice != "4");
 
-}
+// using System;
+// using System.Collections.Generic;
+// class Journal
+// {
+//     public string JournalFile = "Journal.txt";
 
 
 
-public string MenuAction()
-{
-    bool validChoice = false;
-    string choice = "";
+// public void Menu()
+// {
+//     string choice;
+//     do
+//     {
+//         choice = MenuAction();
+//         switch (choice)
+//         {
+//             case "1":
+//                 DislpayJournal();
+//                 break;
+//             case "2":
+//                 DeleteJournal();
+//                 break;
+//             case "3":
+//                 AddEntry();
+//                 break;
 
-    do
-    {
-        System.Console.WriteLine("What would you like to do? ");
-        System.Console.WriteLine("1. Read Journal ");
-        System.Console.WriteLine("2. Delete Journal ");
-        System.Console.WriteLine("3. Write in Journal ");
-        System.Console.WriteLine("4. Close Journal ");
+//         }
+//     }
+//     while (choice != "4");
 
-        choice = Console.ReadLine().Trim();
+// }
 
-        if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
-        {
-            validChoice = true;
-        }
-        else
-        {
-            System.Console.WriteLine("I'm sorry but that is an invalid choice. Please select 1-4 from the menu, thank you!");
-        }
 
-    } 
+
+// public string MenuAction()
+// {
+//     bool validChoice = false;
+//     string choice = "";
+
+//     do
+//     {
+//         System.Console.WriteLine("What would you like to do? ");
+//         System.Console.WriteLine("1. Read Journal ");
+//         System.Console.WriteLine("2. Delete Journal ");
+//         System.Console.WriteLine("3. Write in Journal ");
+//         System.Console.WriteLine("4. Close Journal ");
+
+//         choice = Console.ReadLine().Trim();
+
+//         if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
+//         {
+//             validChoice = true;
+//         }
+//         else
+//         {
+//             System.Console.WriteLine("I'm sorry but that is an invalid choice. Please select 1-4 from the menu, thank you!");
+//         }
+
+//     } 
     
-    while (!validChoice);
-    return choice;
-}
+//     while (!validChoice);
+//     return choice;
+// }
 
 
 
-public void CreateFile()
-{
-    if (!System.IO.File.Exists(JournalFile))
-    {
-        File.CreateText(JournalFile);
-    }
-}
-
-
-
-public void DislpayJournal()
-{
-    System.Console.WriteLine("Welcome to your journal! Write in me some more! ");
+// public void DislpayJournal()
+// {
+//     System.Console.WriteLine("Welcome to your journal! Write in me some more! ");
     
-    string entries = File.ReadAllText(JournalFile);
-    System.Console.WriteLine(entries);
-
-}
-
-
-
-public void AddEntry()
-{
-    System.Console.WriteLine("What's on your mind today? (Type 'thanks' when you're done) ");
-
-    string journalEntry = "";
-    bool keepGoing = true;
-    while (keepGoing)
-    {
-        string line = Console.ReadLine();
-        if (line.ToLower() == "thanks")
-        {
-            keepGoing = false;
-        }
-        else
-        {
-            journalEntry += line;
-        }
-    }
-    File.AppendAllText(JournalFile, $"Entry: {journalEntry}");
-    System.Console.WriteLine("Your Journal has been updated! ");
-
-    return;
-}
+//     {
+//     string entries = File.ReadAllText(JournalFile);
+//     System.Console.WriteLine("Journal");
+//     System.Console.WriteLine(entries);
+//     }
+// }
 
 
 
-public void DeleteJournal()
-{
-File.WriteAllText(JournalFile, String.Empty);
-System.Console.WriteLine("All data in your Journal has been cleared. ");
+// public void AddEntry()
+// {
+//     System.Console.WriteLine("What's on your mind today? (Type 'thanks' when you're done) ");
 
-return;
-}
-}
+//     string journalEntry = "";
+//     bool keepGoing = true;
+//     while (keepGoing)
+//     {
+//         string line = Console.ReadLine();
+//         if (line.ToLower() == "thanks")
+//         {
+//             keepGoing = false;
+//         }
+//         else
+//         {
+//             journalEntry += line;
+//         }
+//     }
+//     File.AppendAllText(JournalFile, $"Entry: {journalEntry}");
+//     System.Console.WriteLine("Your Journal has been updated! ");
+
+//     return;
+// }
+
+
+
+// public void DeleteJournal()
+// {
+// File.WriteAllText(JournalFile, String.Empty);
+// System.Console.WriteLine("All data in your Journal has been cleared. ");
+
+// return;
+// }
+// }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,102 @@
 using System;
+using System.IO;
+
+class Journal
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+    public string JournalFile = "Journal.txt";
+}
 
 class Program
 {
+    static List<string> prompts = new List<string> {
+        "How did you serve someone today? ",
+        "Did you come closer to Christ? ",
+        "What's something you would have changed from today? ",
+        "What do you plan on doing this weekend? ",
+        "What did you study in your scriptures today? "
+    };
+    static List<Journal> journal = new List<Journal>();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        while (true)
+        {
+            Console.WriteLine("1. Write a New Entry. ");
+            Console.WriteLine("2. Display the Journal. ");
+            Console.WriteLine("3. Save Journal to a file. ");
+            Console.WriteLine("4. Delete the Journal. ");
+            Console.WriteLine("5. Exit. ");
+
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                AddEntry();
+            }
+            else if (choice == "2")
+            {
+                DisplayJournal();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Enter the filename: ");
+                string fileName = Console.ReadLine();
+                SaveJournal();
+            }
+            else if (choice == "4")
+            {
+                DeleteJournal();
+            }
+            else if (choice == "5")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    static void AddEntry()
+    {
+        Random randomPrompt = new Random();
+        int index = randomPrompt.Next(prompts.Count);
+        string prompt = prompts[index];
+
+        Console.Write(prompt + " ");
+        string response = Console.ReadLine();
+        
+        Journal entry = new Journal
+        {
+            Prompt = prompt,
+            Response = response,
+            Date = DateTime.Now
+        };
+
+        journal.Add(entry);
+    }
+
+    static void DisplayJournal()
+    {
+        foreach (Journal entry in journal)
+        {
+            Console.WriteLine("Prompt: " + entry.Prompt);
+            Console.WriteLine("Response: " + entry.Response);
+            Console.WriteLine("Date: " + entry.Date);
+        }
+    }
+
+    static void SaveJournal()
+    {
+        File.AppendAllText(JournalFile);
+    }
+
+    static void DeleteJournal()
+    {
+        File.WriteAllText(JournalFile, String.Empty);
+        System.Console.WriteLine("All contents of your Journal has been cleared. ");
     }
 }
