7e5e6e8a559a1282c9aa9219a6242d7682ad7891
samoanspam <thalpatane@byui.edu>
2023-02-01 22:17:53 -0700

    Code says there are tons of errors, still works.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        System.Console.WriteLine(frac1.GetFractionString());
+        System.Console.WriteLine(frac1.GetDecimalValue());
+
+
+        Fraction frac2 = new Fraction(6);
+        System.Console.WriteLine(frac2.GetFractionString());
+        System.Console.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(6,7);
+        System.Console.WriteLine(frac3.GetFractionString());
+        System.Console.WriteLine(frac3.GetDecimalValue());
+
+        Fraction frac4 = new Fraction(1, 3);
+        System.Console.WriteLine(frac4.GetFractionString());
+        System.Console.WriteLine(frac4.GetDecimalValue());
+
     }
 }
