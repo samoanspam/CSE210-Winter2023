891bbf373f4ceadb6b48ca7d9b769e9c2f846b25
samoanspam <thalpatane@byui.edu>
2023-02-28 12:48:06 -0700

    random practices

---
+++ sandbox/Sandbox/BreathingActivity.cs
@@ -0,0 +1,14 @@
+using System;
+
+class BreathingActivity
+{
+    public BreathingActivity() : base()
+    {
+
+    }
+
+       public BreathingActivity(string name, string descripiton, string welcomeMessage, string endingMessage, int duration : base(name, descripiton, welcome, ending, duration))
+    {
+
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,104 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-
-class Person
+class Activity
 {
-    public string name;
-    string birth;
-    string gender;
-    List<string> names;
+    private string name = "";
+    private string descripiton = "";
+    private string welcomeMessage = "";
+    private string endingMessage = "";
+    private int duration = 0;
 
-    public string GetGender()
+    public Activity()
     {
-        return this.gender;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-
-        Person fred = new Person();
-        fred.name = "Fred";
-        System.Console.WriteLine(fred.GetGender());
-
-        List<string> items = new List<string>();
-        List<float> prices = new List<float>();
-
-        bool done = false;
-        while (!done)
-        {
-            DisplayMenu();
-            int choice = GetChoice();
-
-            // switch (choice)
-            // {
-            //     case 1:
-            //         AddItem(items, prices);
-            //         break;
-            //     case 2:
-            //         Display(items, prices);
-            //         break;
-            //     case 5:
-            //         done = false;
-            //         break;
-            // }
-            // if (choice == 1)
-
-            {
-                AddItem(items, prices);
-            }
-
-            if (choice == 2)
-            {
-                Display(items, prices);
-            }
-
-            if (choice == 5)
-            {
-                done = true;
-            }
-        }
+        // nothing to do here since arguements/variables are declared above.
     }
 
-    static void DisplayMenu()
+    public Activity(string name, string descripiton, string welcomeMessage, string endingMessage, int duration)
     {
-        System.Console.WriteLine("1) Add Item");
-        System.Console.WriteLine("2) ADisplay");
-        System.Console.WriteLine("3) Total");
-        System.Console.WriteLine("4) Remove");
-        System.Console.WriteLine("5) Quit");
+        _name = name;
+        _description = descripiton;
+        _welcome = welcome;
+        _ending = ending;
+        _duration = duration;
     }
-
-    static int GetChoice()
-    {
-        System.Console.Write("Enter Choice: ");
-        string choice = Console.ReadLine();
-        int answer = int.Parse(choice);
-        return answer;
-    }
-    static void AddItem(List<string> items, List<float> prices)
-    {
-        System.Console.Write("Enter Item: ");
-        string item = Console.ReadLine();
-
-        System.Console.Write("Enter Item: ");
-        string price = Console.ReadLine();
-        float priceAmount = float.Parse(price);
-
-        items.Add(item);
-        prices.Add(priceAmount);
-    }
-
-    static void Display(List<string> items, List<float> prices)
-    {
-        System.Console.WriteLine("DISPLAY ITEMS");
-        for (int i = 0; i < items.Count; i++)
-            {
-                System.Console.WriteLine($"{items[i]}: price{prices[i]}");
-            }
-        System.Console.WriteLine("");
-    }
-
 }
